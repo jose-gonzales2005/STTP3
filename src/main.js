d1be34b52633e9fabc48f75b2a723421298864ab
Noah Dane <ndane@ucsc.edu>
2024-03-23 17:02:28 -1000

    main.js message and screwing w turkey sprite on accident

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@
 /* credit mentioned in the instruction scene as well
 Credit to: Nathan Altice, for moving particle emitter, gravity well, and particle death zone code
 Jack Morehart (friend) for significant assistance in setting up HTML Dom code for shop
-Credit to 
+
+Major Phaser Components Used: Physics, Particle Effects, Tweens, Text object, Timer
+*Particulary happy with how the Upgrades Shop came out, my initial idea had a few more upgrades, enough 
+to scroll through a page or two, which seemed daunting in Phaser/JS so I implemented a DOM element with 
+HTML to represent the shop menu. Nathan told me he'd never seen anyone do this, which was a little 
+concerning, but I figured out the communcation between the js code and html dom with not an insane
+amount of trouble. 
 */
 
 'use strict'
